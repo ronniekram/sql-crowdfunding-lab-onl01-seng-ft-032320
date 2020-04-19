484b57059cd1679464dcc630e03a0118d209639f
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-19 23:19:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,13 +22,12 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
     GROUP BY users.name"
   end
   
-  def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-    "SELECT projects.title, pledges.amount
+   def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+    "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
     FROM projects
     JOIN pledges
     ON projects.id = pledges.project_id
-    WHERE pledges.amount - projects.funding_goal >= 0
-    ORDER BY pledges.amount - projects.funding_goal"
+    GROUP BY projects.title HAVING SUM(pledges.amount) - projects.funding_goal >=0"
   end
   
   def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
